7b49cca9cf59c4c3d7b7d10cd0e935ba6f607ad8
Corentin <corentin.gendreau@orange.fr>
2024-09-10 20:04:58 +0200

    un peu de doc ne fait jamais de mal

--- algorithmes classiques/Recherche/Recherche d'un élément dans une liste/recherche_element.py
+++ algorithmes classiques/Recherche/Recherche d'un élément dans une liste/recherche_element.py
@@ -1,4 +1,15 @@
 def recherche(liste, valeur):
+    '''
+    Recherche un élément dans une liste fournie
+
+    Entree :
+    liste : liste de valeurs
+    valeur : valeur à rechercher dans la liste
+
+    Retour :
+    liste des positions de la valeur recherchée dans la liste
+    [] si la valeur n'est pas trouvée dans la liste
+    '''
     return [i for i in range(len(liste)) if liste[i] == valeur]
 
 liste=[1,2,3,4,1,2,5,6,1,3,4,8,1,9,3,2,6,7,4,1,2]
