42a034b3c6f32998768ef63076759b30a716d7ec
DESKTOP-1BM8KFM\cogsm <cog.s.moi@gmail.com>
2024-09-11 23:10:38 +0200

    ajout de la décomposition binaire 'normale' sans récursivité

--- algorithmes classiques/Decomposition/Decomposition binaire/decomposition_binaire.py
+++ algorithmes classiques/Decomposition/Decomposition binaire/decomposition_binaire.py
@@ -1,6 +1,6 @@
 def decomposition_binaire_rec(nombre):
     '''
-    Ressort la valeur binaire d'un nombre donné en entrée
+    Ressort la valeur binaire d'un nombre donné en entrée (de manière récursive)
 
     Entrée :
     nombre (int) : nombre à décomposer
@@ -14,3 +14,22 @@ def decomposition_binaire_rec(nombre):
     return decomposition_binaire_rec(nombre//2)+[nombre%2]
 
 print(decomposition_binaire_rec(6))
+
+def decomposition_binaire(nombre):
+    '''
+    Ressort la valeur binaire d'un nombre donné en entrée
+
+    Entrée :
+    nombre (int) : nombre à décomposer
+
+    Sortie :
+    int[] : décomposition binaire du nombre
+    '''
+    res=[]
+    while nombre>1:
+        res=[nombre%2]+res
+        nombre=nombre//2
+    res=[nombre]+res
+    return res
+
+print(decomposition_binaire(6))
--- algorithmes classiques/Decomposition/Decomposition binaire/tests.py
+++ algorithmes classiques/Decomposition/Decomposition binaire/tests.py
@@ -1,6 +1,11 @@
-from decomposition_binaire import decomposition_binaire_rec
+from decomposition_binaire import decomposition_binaire_rec, decomposition_binaire
 
-def test_decomposition():
+def test_decomposition_rec():
     nombre=45
     expected_output=[1,0,1,1,0,1]
     assert decomposition_binaire_rec(nombre)==expected_output
+
+def test_decomposition_normale():
+    nombre=45
+    expected_output=[1,0,1,1,0,1]
+    assert decomposition_binaire(nombre)==expected_output
