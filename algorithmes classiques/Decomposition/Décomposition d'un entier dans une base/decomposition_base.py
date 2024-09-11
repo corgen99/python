29c28e486bd430fe5be1b6c8d645986530a646ed
DESKTOP-1BM8KFM\cogsm <cog.s.moi@gmail.com>
2024-09-11 23:15:25 +0200

    Ajout de la version normale de la décomposition

--- algorithmes classiques/Decomposition/Décomposition d'un entier dans une base/decomposition_base.py
+++ algorithmes classiques/Decomposition/Décomposition d'un entier dans une base/decomposition_base.py
@@ -15,3 +15,23 @@ def decomposition_base_rec(nombre,base):
     return decomposition_base_rec(nombre//base, base)+[nombre%base]
 
 print(decomposition_base_rec(23,8))
+
+def decomposition_base(nombre,base):
+    '''
+    Ressort la décomposition d'un nombre donné dans la base voulue
+
+    Entrée :
+    nombre (int) : nombre à décomposer
+    base (int) : base de la décomposition
+
+    Sortie :
+    int[] : décomposition binaire du nombre
+    '''
+    res=[]
+    while nombre>base:
+        res=[nombre%base]+res
+        nombre=nombre//base
+    res=[nombre]+res
+    return res
+
+print(decomposition_base(23,8))
--- algorithmes classiques/Decomposition/Décomposition d'un entier dans une base/tests.py
+++ algorithmes classiques/Decomposition/Décomposition d'un entier dans une base/tests.py
@@ -1,7 +1,13 @@
-from decomposition_base import decomposition_base_rec
+from decomposition_base import decomposition_base_rec, decomposition_base
+
+def test_decomposition_rec():
+    nombre=90
+    base=25
+    expected_output=[3,15]
+    assert decomposition_base_rec(nombre,base)==expected_output
 
 def test_decomposition():
     nombre=90
     base=25
     expected_output=[3,15]
-    assert decomposition_base_rec(nombre,base)==expected_output
+    assert decomposition_base(nombre,base)==expected_output
