239782d8082623f7995f3c843c7fa825444c7022
DESKTOP-1BM8KFM\cogsm <cog.s.moi@gmail.com>
2024-09-11 23:02:31 +0200

    correction de quelques erreurs

--- algorithmes classiques/Decomposition/Decomposition binaire/decomposition_binaire.py
+++ algorithmes classiques/Decomposition/Decomposition binaire/decomposition_binaire.py
@@ -6,12 +6,11 @@ def decomposition_binaire_rec(nombre):
     nombre (int) : nombre à décomposer
 
     Sortie :
-    int[0] : décomposition binaire du nombre
+    int[] : décomposition binaire du nombre
     '''
     if nombre<=1:
         return [nombre]
     
-    print(nombre)
     return decomposition_binaire_rec(nombre//2)+[nombre%2]
 
 print(decomposition_binaire_rec(6))
