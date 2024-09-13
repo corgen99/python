2dedd2b0339cafd53e1647a9adb77339945fd914
Corentin <corentin.gendreau@orange.fr>
2024-09-14 00:51:30 +0200

    ajout de la recherche des valeurs possibles pour une case donnée

--- algorithmes classiques/Jeu/Sudoku/gameplay.py
+++ algorithmes classiques/Jeu/Sudoku/gameplay.py
@@ -11,3 +11,42 @@ def placer_nombre(grille,chiffre,ligne,colonne):
     '''
     grille[ligne][colonne]=chiffre
     return grille
+
+def nombres_ligne(grille, ligne):
+    '''
+    renvoie les nombres dans une ligne donnée
+    '''
+    res=[]
+    for i in range(9):
+        if grille[ligne][i]>0:
+            res.append(grille[ligne][i])
+    return res
+
+def nombres_colonne(grille,colonne):
+    '''
+    renvoie les nombres dans une colonne donnée
+    '''
+    res=[]
+    for i in range(9):
+        if grille[i][colonne]>0:
+            res.append(grille[i][colonne])
+    return res
+
+def nombres_carre(grille,carre):
+    '''
+    renvoie les nombres présents dans le carré donné
+    '''
+    res=[]
+    for i in range(3):
+        for j in range(3):
+            if grille[(carre//3)*3+i][(carre%3)*3+j]>0:
+                res.append(grille[(carre//3)*3+i][(carre%3)*3+j])
+    return res
+
+def chercher_valeurs_possible(grille, ligne, colonne):
+    '''
+    cherche les valeurs possibles d'une case donnée
+    '''
+    numero_carre=3*(ligne//3)+(colonne//3)
+    nombres_indisponible=nombres_ligne(grille,ligne)+nombres_colonne(grille,colonne)+nombres_carre(grille,numero_carre)
+    return [i for i in range(1,9) if i not in nombres_indisponible ]
--- algorithmes classiques/Jeu/Sudoku/test_gameplay.py
+++ algorithmes classiques/Jeu/Sudoku/test_gameplay.py
@@ -1,6 +1,18 @@
 from creation import *
 from gameplay import *
 
+
+
+grille=[[5,6,3,8,7,9,2,1,4],\
+        [7,1,9,4,2,3,6,5,8],\
+        [2,8,4,5,6,1,3,9,7],\
+        [4,2,6,1,5,7,9,8,3],\
+        [2,9,5,6,3,8,4,7,2],\
+        [8,3,7,2,9,4,1,6,5],\
+        [9,4,8,3,1,5,7,2,6],\
+        [6,5,1,7,4,2,8,3,9],\
+        [3,7,2,9,8,6,5,4,1]]
+
 def test_ajouter_nombre():
     valeur=4
     ligne=2
@@ -20,3 +32,32 @@ def test_ajouter_nombre():
         [0,0,0,0,0,0,0,0,0]]
 
     assert grille==expected_value
+
+def test_nombres_ligne():
+    ligne=3
+    expected_value=[4,2,6,1,5,7,9,8,3]
+    assert nombres_ligne(grille,ligne)==expected_value
+
+def test_nombres_colonne():
+    colonne=6
+    expected_value=[2,6,3,9,4,1,7,8,5]
+    assert nombres_colonne(grille,colonne)==expected_value
+
+def test_nombres_carre():
+    carre=2
+    expected_value=[2,1,4,6,5,8,3,9,7]
+    assert nombres_carre(grille,carre)==expected_value
+
+def test_nombres_possible():
+    grille=[
+        [5,6,3,8,7,9,2,1,4],\
+        [0,1,9,4,2,3,6,5,0],\
+        [2,0,4,5,6,1,3,9,7],\
+        [4,2,6,1,5,7,9,8,3],\
+        [2,9,5,6,3,8,4,7,2],\
+        [0,3,7,2,9,4,1,6,5],\
+        [9,4,8,3,1,5,7,2,6],\
+        [6,5,1,7,4,2,8,3,9],\
+        [3,7,2,9,8,6,5,4,1]]
+    expected_value=[7,8]
+    assert chercher_valeurs_possible(grille,1,0)==expected_value
