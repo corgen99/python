0ab175233077e96d54a902ccee98643731996480
Corentin <corentin.gendreau@orange.fr>
2024-09-14 21:56:03 +0200

    il n'y a plus d'erreurs mais certaines lignes ne finissent pas

--- algorithmes classiques/Jeu/Sudoku/creation.py
+++ algorithmes classiques/Jeu/Sudoku/creation.py
@@ -74,66 +74,151 @@ def valeurs_dispo_ligne(grille, numero_ligne):
 #         random.shuffle(liste_nombres)
 #         return liste_nombres[0]
 
-def selection_ligne(ligne):
-    def controle_nombre():
-        def compte_nombre(nombre):
-            count=0
-            for i in range(9):
-                if nombre in ligne[i]:
-                    count+=1
-            return count
-        controle=[]
-        for i in range(1,10):
-            controle.append(compte_nombre(i))
-        return controle
+# def selection_ligne(ligne):
+#     def controle_nombre():
+#         def compte_nombre(nombre):
+#             count=0
+#             for i in range(9):
+#                 if nombre in ligne[i]:
+#                     count+=1
+#             return count
+#         controle=[]
+#         for i in range(1,10):
+#             controle.append(compte_nombre(i))
+#         return controle
+         
+#     def recherche_uniques():
+#         '''
+#         Recherche des nombres seuls et les retire des autres sous-chaines
+#         '''
+#         def retirer_valeur(valeur,indice):
+#             """
+#             recherche les autres occurences de la valeur dans la liste
+#             """
+#             for j in range(9): #on balaye toutes les sous-listes
+#                 if j!=indice and valeur in ligne[j]: #si la valeur est dans la ligne de j et que j n'est pas l'indice recherché
+#                     ligne[j].remove(valeur)
+#                     if len(ligne[j])==1:
+#                         cases_a_traiter.append(j)
+        
+#         for i in cases_a_traiter: #on balaye les cases à traiter
+#             cases_a_traiter.remove(i)
+#             case_traitee.append(i)
+#             retirer_valeur(ligne[i], i)
     
-    def nombres_a_traiter():
-        return [i for i in range(9) if compte[i]>1]
-     
-    def recherche_uniques():
-        '''
-        Recherche des nombres seuls et les retire des autres sous-chaines
-        '''
-        def retirer_valeur(valeur,indice):
-            """
-            recherche les autres occurences de la valeur dans la liste
-            """
-            for j in range(9): #on balaye toutes les sous-listes
-                if j!=indice and valeur in ligne[j]: #si la valeur est dans la ligne de j et que j n'est pas l'indice recherché
-                    ligne[j].remove(valeur)
-                    compte[valeur-1]-=1
+#     # compte le nombre d'occurrence de chaque nombre dans les différentes sous-listes, pour s'assurer qu'il n'y en a pas un qui est le dernier disponible
+#     compte=controle_nombre()
 
-        for i in range(9): #on balaye les sous-listes
-            if len(ligne[i])==1 and not traite[ligne[i]-1]: #si la sous liste est de taille 1 et que le nombre dedans n'a pas été traité
-                traite[ligne[i]-1]=True #on signale qu'on l'a traité
-                retirer_valeur(ligne[i], i)
+#     # les cases a traiter en priorité sont celles ou une seule valeur est disponible
+#     cases_a_traiter=[i for i in range(9) if len(ligne[i])==1]
+#     case_traitee=[]
     
-    traite=[]
-    for i in range(9):
-        traite.append(False)
+#     # on initialise en cherchant les nombres uniques dans leur ligne ayant pas encore été traités
+#     recherche_uniques()
+#     for i in range(9):
+#         if i not in case_traitee: #si la case n'est pas traitée
+#             random.shuffle(ligne[i])
+#             ligne[i]=[ligne[i].pop()]
+#             cases_a_traiter.append(i)
+#             recherche_uniques()
+    
+#     return ligne
 
-    compte=controle_nombre()
-    traite=nombres_a_traiter()
 
+
+def rechercher_uniques(ligne):
+    """
+    recherche les cases contenant une seule valeur
+    """
+    return [i for i in range(len(ligne)) if len(ligne[i])==1]
+
+def compte_nombre(ligne):
+    """
+    compte le nombre d'occurrence de chaque nombre dans une ligne donnée
+    """
+    res=[0]*9
+    for i in range(len(ligne)):
+        for j in range(len(ligne[i])):
+            res[ligne[i][j]-1]+=1
+    return res
+
+def rechercher_nombre_unique(ligne):
+    """
+    recherche les nombres n'apparaissant que dans une case
+    """
+    compte=compte_nombre(ligne)
+    return [i+1 for i in range(len(ligne)) if compte[i]==1]
     
-    recherche_uniques()
-    for i in range(9):
-        if len(ligne[i])>1:
-            random.shuffle(ligne[i])
-            ligne[i]=[ligne[i].pop()]
-            recherche_uniques()
-    
+def valeur_aleatoire(sous_ligne):
+    """
+    prend une valeur aléatoire dans une sous-ligne
+    """
+    random.shuffle(sous_ligne)
+    return sous_ligne.pop()
+
+def retirer_valeurs(ligne,valeur):
+    for sous_ligne in ligne:
+        if len(sous_ligne)>1 and valeur in sous_ligne:
+            sous_ligne.remove(valeur)
     return ligne
 
-[4,8,2,9,6,3,7,1,5]
+def chercher(ligne, valeur):
+    for i in range(len(ligne)):
+        if valeur in ligne[i]:
+            return i
+    return -1
 
+def creation_ligne(ligne):
+    """
+    crée une ligne à partir d'une liste contenant la liste des valeurs disponibles pour chaque case
+    """
+    liste=[0]*9
+    valeurs_traites=[]
+    cases_traitees=[]
+    traitement=True
+
+    while len(cases_traitees)!=len(ligne) and traitement: #Itérer tant qu'on n'a pas traité toutes les cases
+        traitement=False
+        cases_uniques=rechercher_uniques(ligne)
+        for i in cases_uniques:
+            if i not in cases_traitees: #on la traite
+                cases_traitees.append(i) #on indique que la case est traitée
+                valeurs_traites.append(ligne[i]) #on indique que la valeur est traitée
+                retirer_valeurs(ligne,ligne[i])
+                liste[i]=ligne[i][0]
+                traitement=True
+        
+        valeurs_uniques=rechercher_nombre_unique(ligne)
+        for i in valeurs_uniques:
+            if i not in valeurs_traites: #on la traite
+                case_a_traiter=chercher(ligne,i)
+                cases_traitees.append(case_a_traiter)
+                valeurs_traites.append(i)
+                ligne[case_a_traiter]=[i]
+                liste[case_a_traiter]=i
+                retirer_valeurs(ligne,i)
+                traitement=True
+        
+        if not traitement:
+            for i in range(len(ligne)):
+                if i not in cases_traitees and not traitement:
+                    valeur=valeur_aleatoire(ligne[i])
+                    cases_traitees.append(i)
+                    valeurs_traites.append(valeur)
+                    ligne[i]=[valeur]
+                    liste[i]=valeur
+                    retirer_valeurs(ligne,valeur)
+                    traitement=True
+        
+    return liste
+    
 def creer_ligne(grille, numero_ligne):
     '''
     crée la ligne demandée
     '''
     ligne=valeurs_dispo_ligne(grille,numero_ligne)
-    print(ligne)
-    ligne=selection_ligne(ligne)
+    # print(ligne)
+    ligne=creation_ligne(ligne)
     return ligne
 
 # def creer_colonne(grille,numero_colonne):
@@ -163,6 +248,6 @@ def creer_grille():
         nouvelle_ligne=creer_ligne(grille,i)
         print(f"Nouvelle ligne : {nouvelle_ligne}")
         for j in range(9):
-            grille=placer_nombre(grille,nouvelle_ligne[j][0],i,j)
+            grille=placer_nombre(grille,nouvelle_ligne[j],i,j)
     
     return grille
--- algorithmes classiques/Jeu/Sudoku/sudoku.py
+++ algorithmes classiques/Jeu/Sudoku/sudoku.py
@@ -3,58 +3,58 @@ from affichage import print_belle_grille
 from creation import creer_grille
 from verification import verifier_grille
 
-grille_1=[\
-        [0,5,0,0,2,0,9,0,0],\
-        [0,0,0,9,0,0,0,1,5],\
-        [0,9,0,5,1,4,0,3,0],\
-        [0,7,0,0,5,6,1,0,0],\
-        [0,4,5,0,0,0,2,9,0],\
-        [0,0,3,2,4,0,0,8,0],\
-        [0,3,0,1,8,7,0,2,0],\
-        [4,8,0,0,0,2,0,0,0],\
-        [0,0,2,0,3,0,0,7,0],\
-        ] #entrer ici la grille à résoudre
+# grille_1=[\
+#         [0,5,0,0,2,0,9,0,0],\
+#         [0,0,0,9,0,0,0,1,5],\
+#         [0,9,0,5,1,4,0,3,0],\
+#         [0,7,0,0,5,6,1,0,0],\
+#         [0,4,5,0,0,0,2,9,0],\
+#         [0,0,3,2,4,0,0,8,0],\
+#         [0,3,0,1,8,7,0,2,0],\
+#         [4,8,0,0,0,2,0,0,0],\
+#         [0,0,2,0,3,0,0,7,0],\
+#         ] #entrer ici la grille à résoudre
 
-print("Grille initiale :")
-print_belle_grille(grille_1)
+# print("Grille initiale :")
+# print_belle_grille(grille_1)
 
-resoudre_intuitif(grille_1)
-print()
+# resoudre_intuitif(grille_1)
+# print()
 
-grille_2=[\
-        [0,0,0,0,8,0,0,0,0],\
-        [0,0,0,0,0,0,4,0,6],\
-        [9,0,7,5,0,2,0,0,0],\
-        [0,2,0,0,0,1,0,0,8],\
-        [0,0,9,8,3,0,5,0,0],\
-        [0,6,0,0,0,4,0,0,9],\
-        [1,0,8,3,0,9,0,0,0],\
-        [0,0,0,0,0,0,8,0,2],\
-        [0,0,0,0,7,0,0,0,0],\
-        ] #entrer ici la grille à résoudre
+# grille_2=[\
+#         [0,0,0,0,8,0,0,0,0],\
+#         [0,0,0,0,0,0,4,0,6],\
+#         [9,0,7,5,0,2,0,0,0],\
+#         [0,2,0,0,0,1,0,0,8],\
+#         [0,0,9,8,3,0,5,0,0],\
+#         [0,6,0,0,0,4,0,0,9],\
+#         [1,0,8,3,0,9,0,0,0],\
+#         [0,0,0,0,0,0,8,0,2],\
+#         [0,0,0,0,7,0,0,0,0],\
+#         ] #entrer ici la grille à résoudre
 
-print("Grille initiale :")
-print_belle_grille(grille_2)
+# print("Grille initiale :")
+# print_belle_grille(grille_2)
 
-resoudre_intuitif(grille_2)
-print()
+# resoudre_intuitif(grille_2)
+# print()
 
-grille_3=[\
-        [3,4,5,0,0,0,0,0,8],\
-        [6,1,0,0,8,3,5,4,9],\
-        [7,9,0,0,4,5,0,0,6],\
-        [0,0,0,1,5,7,0,0,0],\
-        [0,0,0,0,6,4,9,0,0],\
-        [0,7,1,9,0,0,4,0,0],\
-        [0,0,9,0,2,0,6,0,4],\
-        [0,5,0,0,1,0,0,0,0],\
-        [0,0,6,0,0,0,3,0,0],\
-        ] #entrer ici la grille à résoudre
+# grille_3=[\
+#         [3,4,5,0,0,0,0,0,8],\
+#         [6,1,0,0,8,3,5,4,9],\
+#         [7,9,0,0,4,5,0,0,6],\
+#         [0,0,0,1,5,7,0,0,0],\
+#         [0,0,0,0,6,4,9,0,0],\
+#         [0,7,1,9,0,0,4,0,0],\
+#         [0,0,9,0,2,0,6,0,4],\
+#         [0,5,0,0,1,0,0,0,0],\
+#         [0,0,6,0,0,0,3,0,0],\
+#         ] #entrer ici la grille à résoudre
 
-print("Grille initiale :")
-print_belle_grille(grille_3)
+# print("Grille initiale :")
+# print_belle_grille(grille_3)
 
-resoudre_intuitif(grille_3)
+# resoudre_intuitif(grille_3)
 
 grille=creer_grille()
 print("Grille :")
