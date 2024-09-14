bc7af685bfeff5452a74d3027d25c21c5c233e40
Corentin <corentin.gendreau@orange.fr>
2024-09-14 14:58:54 +0200

    developpement de la création de nouvelles grilles

--- algorithmes classiques/Jeu/Sudoku/creation.py
+++ algorithmes classiques/Jeu/Sudoku/creation.py
@@ -1,6 +1,10 @@
 '''
 creation d'une nouvelle grille aleatoire
 '''
+import random
+from verification import *
+from gameplay import placer_nombre, nombres_carre, nombres_ligne, nombres_colonne
+from affichage import print_belle_grille
 
 def nouvelle_grille():
     '''
@@ -11,4 +15,101 @@ def nouvelle_grille():
         grille.append([])
         for j in range(9):
             grille[i].append(0)
+    return grille
+
+def nombre_aleatoire():
+    return random.randint(1,9)
+
+def nombres_dispo(grille, ligne_actuelle, numero_ligne, numero_colonne):
+    '''
+    renvoie les nombres disponibles
+    '''
+    colonne=nombres_colonne(grille, numero_colonne)
+    ligne=nombres_ligne(grille, numero_ligne)
+    carre=nombres_carre(grille, (numero_ligne//3)*3+numero_colonne//3)
+    liste=colonne+ligne+carre+ligne_actuelle
+
+    liste_dispo=[]
+    for i in range(1,10):
+        if i not in liste:
+            liste_dispo.append(i)
+    return liste_dispo
+
+def nombres_dispo(grille, numero_ligne, numero_colonne):
+    '''
+    renvoie les nombres disponibles
+    '''
+    if grille[numero_ligne][numero_colonne]:
+        return [grille[numero_ligne][numero_colonne]]
+    
+    colonne=nombres_colonne(grille, numero_colonne)
+    ligne=nombres_ligne(grille, numero_ligne)
+    carre=nombres_carre(grille, (numero_ligne//3)*3+numero_colonne//3)
+    liste=colonne+ligne+carre
+
+    liste_dispo=[]
+    for i in range(1,10):
+        if i not in liste:
+            liste_dispo.append(i)
+    return liste_dispo
+
+def valeurs_dispo_ligne(grille, numero_ligne):
+    '''
+    renvoie la liste des valeurs dispo pour toutes les lignes
+    '''
+    res=[]
+    for i in range(9):
+        disponibles=nombres_dispo(grille, numero_ligne, i)
+        res.append(disponibles)
+    return res
+
+def nouveau_nombre(grille, ligne_actuelle, numero_ligne, numero_colonne):
+    '''
+    trouve un nouveau nombre répondant aux contraintes
+    '''
+    if grille[numero_ligne][numero_colonne] != 0:
+        return grille[numero_ligne][numero_colonne]
+    else:
+        liste_nombres=nombres_dispo(grille, ligne_actuelle, numero_ligne, numero_colonne)
+        random.shuffle(liste_nombres)
+        print(liste_nombres)
+        return liste_nombres[0]
+        
+
+def creer_ligne(grille, numero_ligne):
+    '''
+    crée la ligne demandée
+    '''
+    ligne=[]
+    for i in range(9):
+        ligne.append(nouveau_nombre(grille, ligne, numero_ligne, i))
+        print(f"Ligne {numero_ligne}, valeur : {ligne}")
+    return ligne
+
+# def creer_colonne(grille,numero_colonne):
+#     '''
+#     crée la colonne demandée
+#     '''
+#     colonne=[]
+#     for i in range(9):
+#         colonne.append(nouveau_nombre(grille, i, numero_colonne))
+#     return colonne
+
+def creer_grille():
+    '''
+    creation d'une grille
+    '''
+    grille=nouvelle_grille()
+    premier_bloc=[1,2,3,4,5,6,7,8,9]
+    random.shuffle(premier_bloc)
+    for i in range(9):
+        grille=placer_nombre(grille,premier_bloc[i],i//3,i%3)
+    
+    print_belle_grille(grille)
+    for i in range(9):
+        # nouvelle_ligne=creer_ligne(grille,i)
+        # for j in range(9):
+        #     grille=placer_nombre(grille,nouvelle_ligne[j],i,j)
+        print(f"Ligne {i}, valeurs disponibles : {valeurs_dispo_ligne(grille, i)}")
+    
     return grille
--- algorithmes classiques/Jeu/Sudoku/sudoku.py
+++ algorithmes classiques/Jeu/Sudoku/sudoku.py
@@ -1,7 +1,9 @@
 from gameplay import resoudre_intuitif
 from affichage import print_belle_grille
+from creation import creer_grille
+from verification import verifier_grille
 
-grille=[\
+grille_1=[\
         [0,5,0,0,2,0,9,0,0],\
         [0,0,0,9,0,0,0,1,5],\
         [0,9,0,5,1,4,0,3,0],\
@@ -14,12 +16,12 @@ grille=[\
         ] #entrer ici la grille à résoudre
 
 print("Grille initiale :")
-print_belle_grille(grille)
+print_belle_grille(grille_1)
 
-resoudre_intuitif(grille)
+resoudre_intuitif(grille_1)
 print()
 
-grille=[\
+grille_2=[\
         [0,0,0,0,8,0,0,0,0],\
         [0,0,0,0,0,0,4,0,6],\
         [9,0,7,5,0,2,0,0,0],\
@@ -32,12 +34,12 @@ grille=[\
         ] #entrer ici la grille à résoudre
 
 print("Grille initiale :")
-print_belle_grille(grille)
+print_belle_grille(grille_2)
 
-resoudre_intuitif(grille)
+resoudre_intuitif(grille_2)
 print()
 
-grille=[\
+grille_3=[\
         [3,4,5,0,0,0,0,0,8],\
         [6,1,0,0,8,3,5,4,9],\
         [7,9,0,0,4,5,0,0,6],\
@@ -50,6 +52,11 @@ grille=[\
         ] #entrer ici la grille à résoudre
 
 print("Grille initiale :")
-print_belle_grille(grille)
+print_belle_grille(grille_3)
 
-resoudre_intuitif(grille)
+resoudre_intuitif(grille_3)
+
+grille=creer_grille()
+print("Grille :")
+print_belle_grille(grille)
+print(f"Le sudoku est valide : {verifier_grille(grille)}")
